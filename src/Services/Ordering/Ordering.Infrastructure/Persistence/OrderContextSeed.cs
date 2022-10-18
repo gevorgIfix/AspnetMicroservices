8fa3600f121644ab9eb3d0e825ba51c6ea689100
gevorgIfix <gevorg.melikyan@ifixgroup.com>
2022-10-18 18:37:40 +0400

    ADD INFSTRACTURE CONTEXT AND REPOS

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "TestuserFName", LastName = "TestuserSName", EmailAddress = "somemail@gmail.com", AddressLine = "TestAddres", Country = "TestCounty", TotalPrice = 350 },
+                new Order() {UserName = "swn", FirstName = "TestuserFName", LastName = "TestuserSName", EmailAddress = "somemail@gmail.com", AddressLine = "TestAddres", Country = "TestCounty", TotalPrice = 450 }
+            };
+        }
+    }
+}
